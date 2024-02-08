89ba904a41aad80b640ad993a9faf308330e4c9b
Nana <57479728+catatankuliah-lab@users.noreply.github.com>
2024-02-09 05:14:41 +0700

    Update dashboard.js

--- gudang/dashboard/dashboard.js
+++ gudang/dashboard/dashboard.js
@@ -1,21 +1,7 @@
-document.onkeydown = function(e) {
-    if(e.keyCode == 123) {
-     return false;
-    }
-    if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)){
-     return false;
-    }
-    if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)){
-     return false;
-    }
-    if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)){
-     return false;
-    }
+document.addEventListener('contextmenu', function(e) {
+        e.preventDefault();
+    });
 
-    if(e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)){
-     return false;
-    }      
- }
 $.ajax({
   url: "http://localhost:8080/api/alokasi",
   method: "GET",
